f2d886dafe78e232dcf4955c6eec58aed0f933fe
AntoinePiron <pro.piron.antoine@gmail.com>
2021-10-21 10:30:06 +0200

    ajout de la deuxieme méthode

--- process.py
+++ process.py
@@ -1,9 +1,24 @@
 from multiprocessing import Process, process
 
+#Deuxième méthode en définissant une clase
+class Greet(Process):
+    def __init__(self, name):
+        super().__init__()
+        self.name = name
+    def run(self):
+        print("Hello, ", self.name, " !")
+
+#Première technique en définisssant une méthode
 def greet(name):
     print("Hello, ", name, "!")
 
 if __name__ == "__main__":
+    #Première méthode
     p = Process(target=greet, args=("Kitty",))
     p.start()
     p.join()
+
+    #Deuxièmre méthode 
+    p2 = Greet("Kitty")
+    p2.start()
+    p2.join()
