fa799db9f24ff20a42640f5b916d059773b25c21
AntoinePiron <pro.piron.antoine@gmail.com>
2021-10-21 11:48:14 +0200

    prog signal

---
+++ signal/signal.py
@@ -0,0 +1,13 @@
+#ATTENTION
+#Il faut executer ce code avec python2 pour le faire fonctionner
+import signal
+
+def handler(sig, frame):
+    if sig == signal.SIGINT:
+        print("}:-)")
+
+signal.signal(signal.SIGINT, handler)
+
+
+while True:
+    pass
