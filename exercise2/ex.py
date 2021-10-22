db15410a0a0c92c16c562e2c013f81f0788e6b05
AntoinePiron <pro.piron.antoine@gmail.com>
2021-10-22 18:41:05 +0200

    exercise 2 done and commented

--- exercise2/ex.py
+++ exercise2/ex.py
@@ -3,16 +3,24 @@ import signal
 from multiprocessing import Process
 import time
 
+#On va créer le process child avec une classe
 class Child(Process):
     def __init__(self):
         super().__init__()
     def run(self):
         time.sleep(5)
+        #On envoie le sigint avec le PID du parent 
         os.kill(os.getppid(), signal.SIGINT)
+        #On le fait rentrer dans un boucle infini pour bien vérifier que c'est le parent qui le kill
+        while True:
+            pass
 
 def handler(sig, frame):
     if sig == signal.SIGINT:
-        print("Ca fonctionne")
+        #On fait un print de debug
+        print("It works")
+        #Puis on kill le process
+        os.kill(p.pid, signal.SIGKILL)
 
 if __name__ == "__main__":
     signal.signal(signal.SIGINT, handler)
