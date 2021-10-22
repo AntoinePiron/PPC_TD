065501eb837cab05a439429449699d477dfa0a25
AntoinePiron <pro.piron.antoine@gmail.com>
2021-10-22 18:58:17 +0200

    compréhension systeme de pipe

---
+++ 04_exercise3/TestPipe.py
@@ -0,0 +1,19 @@
+from multiprocessing import Process, Pipe
+
+class Test(Process):
+    def __init__(self, connection):
+        super().__init__()
+        self.connection = connection
+    def run(self):
+        self.connection.send([42, None, "Hello"])
+        self.connection.close()
+
+
+if __name__ == "__main__":
+    #On génère notre pipe
+    parent_conn, child_conn = Pipe()
+    #On créé alors le process child en lui renseignant sa connection au pipe
+    p = Test(child_conn)
+    p.start()
+    print(parent_conn.recv())
+    p.join()
