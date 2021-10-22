3ccdf03509e8542189e859a3eb07ee3d53679b28
AntoinePiron <pro.piron.antoine@gmail.com>
2021-10-22 18:26:25 +0200

    premiere tentative

--- exercise2/ex.py
+++ exercise2/ex.py
@@ -1,4 +1,21 @@
+import os
+import signal
+from multiprocessing import Process
+import time
 
+class Child(Process):
+    def __init__(self):
+        super().__init__()
+    def run(self):
+        time.sleep(5)
+        os.kill(os.getppid, signal.SIGINT)
+
+def handler(sig, frame):
+    if sig == signal.SIGINT:
+        print("Ca fonctionne")
 
 if __name__ == "__main__":
-    pass
+    signal.signal(signal.SIGINT, handler)
+    p = Child()
+    p.start()
+    p.join()
