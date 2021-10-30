e69fb614e6f22d357c10d96664ac8373b8477ca5
AntoinePiron <pro.piron.antoine@gmail.com>
2021-10-30 18:28:48 +0200

    fin exo3

--- 04_exercise3/exercise3.py
+++ 04_exercise3/exercise3.py
@@ -6,18 +6,22 @@ class Child(Process):
         super().__init__()
         self.connection = connection
     def run(self):
-        #On récupere dans un premier temps la phrase
-        phrase = self.connection.recv()
-        #On la split pour inverser les mots
-        split = phrase.split()
-        endstrng = ""
-        #On parcours le tableau pour inverser les mots
-        for i in range(len(split)):
-            endstrng += split[(len(split)-1)- i] + " "
-        #On les envoies sur le pipe 
-        self.connection.send(endstrng)
-        #On oublie pas de refermer la connection
-        self.connection.close()
+        #On créé ici aussi une boucle infinie
+        while True:
+            #On récupere dans un premier temps la phrase
+            phrase = self.connection.recv()
+            if phrase == "end":
+                self.connection.close()
+                return
+            #On la split pour inverser les mots
+            split = phrase.split()
+            endstrng = ""
+            #On parcours le tableau pour inverser les mots
+            for i in range(len(split)):
+                endstrng += split[(len(split)-1)- i] + " "
+            #On les envoies sur le pipe 
+            self.connection.send(endstrng)
+            #On oublie pas de refermer la connection
 
 
 if __name__ == "__main__":
@@ -27,10 +31,15 @@ if __name__ == "__main__":
     p = Child(child_connection)
     #demarrage du process
     p.start()
-    #On récupere la phrase en input puis on l'envoie
-    send = input("Write a phrase : ")
-    parent_connection.send(send)
-    #On récupere alors la reponse
-    print(parent_connection.recv())
+    #On créé une boucle infini afin de permettre plusieurs envoie
+    while True:
+        #On récupere la phrase en input puis on l'envoie
+        send = input("Write a phrase : ")
+        parent_connection.send(send)
+        if send == "end":
+            print("End of communication...")
+            break
+        #On récupere alors la reponse
+        print(parent_connection.recv())
     parent_connection.close()
     p.join()
