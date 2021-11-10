4e654be218ac4d471c7e4f76eb03c1eb4c960420
AntoinePiron <pro.piron.antoine@gmail.com>
2021-11-10 09:59:26 +0100

    ajout commentaire

--- TD3/03_Exercise2/exercise2.py
+++ TD3/03_Exercise2/exercise2.py
@@ -9,11 +9,12 @@ def worker(in_queue, out_queue, data_ready, data):
         out_queue.put((func.__name__, func(data)))
 
 if __name__ == "__main__":
-
+    #On crée nos queu d'entré et de sortie
     in_queue = Queue()
     out_queue = Queue()
     data_ready = threading.Event()
 
+    #Ce bloc nous permet de générer notre set de data avec l'entrée utilisateur 
     data = []
     #ctrl d pour arreter d'ecrire
     input_str = sys.stdin.read().split()
@@ -25,14 +26,18 @@ if __name__ == "__main__":
             print("bad number", s)
         else:
             data.append(x)
+    #On crée notre thread pret à recevoir les différents fonctions       
     thread = threading.Thread(target=worker, args=(in_queue, out_queue,data_ready, data))
     thread.start()
+    #Nos fonctions 
     ops = [min, max, mean, median, stdev]
+    #On les envoies dans notre queue
     for op in ops:
         in_queue.put(op)
+    #Puis on set() pour lancer les opérations dans le thread 
     data_ready.set()
     thread.join()
-
+    #Un fois le thread finit on boucle sur les résultats 
     for tup in list(out_queue.queue):
         name, value = tup
         print(name, ":", value)
