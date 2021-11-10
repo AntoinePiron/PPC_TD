0fb30879c52894547643e0ae81a7c661c8cfa005
AntoinePiron <pro.piron.antoine@gmail.com>
2021-11-10 09:46:23 +0100

    exo2 okok

--- TD3/03_Exercise2/exercise2.py
+++ TD3/03_Exercise2/exercise2.py
@@ -1,12 +1,35 @@
+import sys
 import threading
 from queue import Queue
+from statistics import mean, median, stdev
 
-def worker(queue, data_ready):
-    pass
+def worker(queue, data_ready, data):
+    data_ready.wait()
+    for elem in list(queue.queue):
+        func = queue.get()
+        print(func.__name__, " : ", func(data))
 
 if __name__ == "__main__":
+
     queue = Queue()
     data_ready = threading.Event()
 
-    thread = threading.Thread(target=worker, args=(queue,data_ready))
+    data = []
+    #ctrl d pour arreter d'ecrire
+    input_str = sys.stdin.read().split()
+    print(input_str)
+    for s in input_str:
+        try:
+            x = float(s)
+        except ValueError:
+            print("bad number", s)
+        else:
+            data.append(x)
+    thread = threading.Thread(target=worker, args=(queue,data_ready, data))
     thread.start()
+    ops = [min, max, mean, median, stdev]
+    for op in ops:
+        queue.put(op)
+    data_ready.set()
+
+    thread.join()
