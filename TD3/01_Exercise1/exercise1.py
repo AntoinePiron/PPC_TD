d5113ae3a3b21bd367b0993c574097d95298c37d
AntoinePiron <pro.piron.antoine@gmail.com>
2021-11-10 08:39:47 +0100

    exercise1 complete

---
+++ TD3/01_Exercise1/exercise1.py
@@ -0,0 +1,22 @@
+import sys
+import threading
+from random import random
+
+def generatePoints(n):
+    global p_in
+    for _ in range(n):
+        x = random()*2 - 1
+        y = random()*2 - 1
+        if (pow(x, 2) + pow(y, 2)) <= 1:
+            p_in += 1
+
+
+
+if __name__ == "__main__":
+    p_in = 0
+    n = int(sys.argv[1])
+    thread = threading.Thread(target=generatePoints, args=(n,))
+    thread.start()
+    thread.join()
+    app_pi = 4*(p_in/n)
+    print("Approximation of PI:", app_pi)
